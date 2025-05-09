3e312ef3f994f8e95e663a30197e87205c2d7372
Lucas <lucas_bezerraroncolato@hotmail.com>
2025-05-09 18:09:38 -0300

    AJuste deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/coopersystem-desafio'),
+      dir: require('path').join(__dirname, './coverage/coopersystem-desafio-tecnico'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'coopersystem-desafio';
+  title = 'coopersystem-desafio-tecnico';
 }
